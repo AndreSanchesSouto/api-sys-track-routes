ecb2981328926dd43a5a576ab3a5764976722252
Wanderley Gabriel Bovi <wanderleygabrielbovi@gmail.com>
2024-10-31 14:04:01 -0300

    fix: district to null

--- src/main/resources/db.migration/V9__create_district.sql
+++ src/main/resources/db.migration/V9__create_district.sql
@@ -1,6 +1,6 @@
 CREATE TABLE district(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    district VARCHAR(100) NOT NULL,
+    district VARCHAR(100) NULL,
     city_fk UUID,
     FOREIGN KEY (city_fk) REFERENCES city(id)
 );
