a3d5dbe32342cf17b038c93089aa74a10022c93b
wanderleygabrielbovi <wanderleygabrielbovi@gmail.com>
2024-11-14 20:06:13 -0300

    rename commit

--- src/main/java/br/com/api_str_innovation/security/Encrypter.java
+++ src/main/java/br/com/api_str_innovation/security/Encrypter.java
@@ -3,7 +3,22 @@ package br.com.api_str_innovation.security;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+public class Encrypter {
 
+    private static MessageDigest messageDigest;
+    private static int BITMASK = 0xff;
+
+    public static String encrypt(String textPlain) {
+        try {
+            if (messageDigest == null) {
+                messageDigest = MessageDigest.getInstance("sha-512");
+            }
+            var hash = messageDigest.digest(textPlain.getBytes());
+            return toHexadecimal(hash);
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException("Encrypter error");
+        }
+    }
 
     private static String toHexadecimal(byte[] hash) {
         var hexString = new StringBuilder();
