229f84e21c381e48b3bb3e89a9cc119f99c08351
wanderleygabrielbovi <wanderleygabrielbovi@gmail.com>
2024-11-14 20:03:37 -0300

    fix: commit name and translate encrypter error

--- src/main/java/br/com/api_str_innovation/security/Encrypter.java
+++ src/main/java/br/com/api_str_innovation/security/Encrypter.java
@@ -16,9 +16,19 @@ public class Encrypter {
             var hash = messageDigest.digest(textPlain.getBytes());
             return toHexadecimal(hash);
         } catch (NoSuchAlgorithmException ex) {
-            throw new RuntimeException("Erro de criptografia");
+            throw new RuntimeException("Encrypter error");
         }
     }
 
-
+    private static String toHexadecimal(byte[] hash) {
+        var hexString = new StringBuilder();
+        for (byte bit : hash) {
+            var hex = Integer.toHexString(BITMASK & bit);
+            if (hex.length() == 1) {
+                hexString.append("0");
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
 }
