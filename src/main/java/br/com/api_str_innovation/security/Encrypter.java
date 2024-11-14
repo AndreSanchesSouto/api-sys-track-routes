ae3944f0d02aa40bfc2eb18e720d0608f250128d
wanderleygabrielbovi <wanderleygabrielbovi@gmail.com>
2024-11-14 20:02:13 -0300

    removing humberto commit

--- src/main/java/br/com/api_str_innovation/security/Encrypter.java
+++ src/main/java/br/com/api_str_innovation/security/Encrypter.java
@@ -20,15 +20,5 @@ public class Encrypter {
         }
     }
 
-    private static String toHexadecimal(byte[] hash) {
-        var hexString = new StringBuilder();
-        for (byte bit : hash) {
-            var hex = Integer.toHexString(BITMASK & bit);
-            if (hex.length() == 1) {
-                hexString.append("0");
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
+
 }
