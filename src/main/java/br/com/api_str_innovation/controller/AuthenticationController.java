8fc7b30cb165ed8fcf4cad7de1d385aa9d1392b3
andresanchessouto <andresanchessouto@gmail.com>
2024-12-01 16:55:37 -0300

    feat: add register and login functionalities

---
+++ src/main/java/br/com/api_str_innovation/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package br.com.api_str_innovation.controller;
+
+import br.com.api_str_innovation.dto.authentication.AuthenticationRequestDTO;
+import br.com.api_str_innovation.dto.employee.ResponseDTO;
+import br.com.api_str_innovation.dto.employee.general_manager.GeneralManagerRequestDTO;
+import br.com.api_str_innovation.service.AuthorizationService;
+import br.com.api_str_innovation.service.GeneralManagerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Autowired
+    private GeneralManagerService service;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> login(@RequestBody @Valid AuthenticationRequestDTO data) {
+        return this.authorizationService.authEmployee(data);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> register(@RequestBody @Valid GeneralManagerRequestDTO data) {
+        return this.service.post(data);
+    }
+
+    @GetMapping
+    public String verify() {
+        return "Okay";
+    }
+
+}
---
+++ src/main/java/br/com/api_str_innovation/security/SecurityFilter.java
@@ -0,0 +1,45 @@
+package br.com.api_str_innovation.security;
+
+import br.com.api_str_innovation.repository.GeneralManagerRepository;
+import br.com.api_str_innovation.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    GeneralManagerRepository generalManagerRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, NullPointerException {
+        var token = this.recoverToken(request);
+        if(token != null) {
+            var login = tokenService.validateToken(token);
+            UserDetails user = generalManagerRepository.findByLogin(login);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
