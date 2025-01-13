85393654008d67a6adcf3d65e94332f5bacef48f
andresanchessouto <andresanchessouto@gmail.com>
2025-01-13 15:49:24 -0300

    test

--- src/main/java/br/com/api_str_innovation/infra/exception/GlobalExceptionHandle.java
+++ src/main/java/br/com/api_str_innovation/infra/exception/GlobalExceptionHandle.java
@@ -15,7 +15,6 @@ public class GlobalExceptionHandle extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserRegisteredException.class)
     private ResponseEntity<ExceptionMessage> userRegisteredHandle(UserRegisteredException exception) {
-
         ExceptionMessage response = new ExceptionMessage(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, exception.getMessage());
         return ResponseEntity.status(response.getStatus()).body(response);
     }
