cbc3bbbb58ecd633ff2687ae784ae6ca290c592b
Wanderley Gabriel Bovi <wanderleygabrielbovi@gmail.com>
2025-06-15 20:50:52 -0300

    fix: style

--- src/main/java/br/com/api_str_innovation/dto/vehicle/VehicleResponseDTO.java
+++ src/main/java/br/com/api_str_innovation/dto/vehicle/VehicleResponseDTO.java
@@ -6,28 +6,26 @@ import br.com.api_str_innovation.entities.vehicle.VehicleEntity;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record VehicleResponseDTO(
-    UUID id,
-    String licensePlateNumber,
-    String sideNumber,
-    String model,
-    String brand,
-    String yearDt,
-    VehicleStatus status,
-    LocalDateTime createdDt,
-    LocalDateTime inactivatedDt
+public record VehicleResponseDTO(UUID id,
+                                String licensePlateNumber,
+                                String sideNumber,
+                                String model,
+                                String brand,
+                                String yearDt,
+                                VehicleStatus status,
+                                LocalDateTime createdDt,
+                                LocalDateTime inactivatedDt
 ) {
     public VehicleResponseDTO(VehicleEntity vehicle) {
-        this(
-            vehicle.getId(),
-            vehicle.getLicensePlateNumber(),
-            vehicle.getSideNumber(),
-            vehicle.getModel(),
-            vehicle.getBrand(),
-            vehicle.getYearDt(),
-            VehicleStatus.valueOf(vehicle.getStatus().toUpperCase()),
-            vehicle.getCreatedDt(),
-            vehicle.getInactivatedDt()
+        this(vehicle.getId(),
+                vehicle.getLicensePlateNumber(),
+                vehicle.getSideNumber(),
+                vehicle.getModel(),
+                vehicle.getBrand(),
+                vehicle.getYearDt(),
+                VehicleStatus.valueOf(vehicle.getStatus().toUpperCase()),
+                vehicle.getCreatedDt(),
+                vehicle.getInactivatedDt()
         );
     }
 }
